4d6f4856347121429d0f141bf9464bd9d5fbc443
Taerogrammer <hyung54321@naver.com>
2025-03-19 19:54:07 +0900

    [setting] 프로젝트 생성

---
+++ MVVM-C/MVVM-C/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM-C
+//
+//  Created by 김태형 on 3/19/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
